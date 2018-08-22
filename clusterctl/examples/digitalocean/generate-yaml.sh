c289e79bffcf94b3255fc169738a642e0d00a740
Marko Mudrinić <mudrinic.mare@gmail.com>
2018-08-22 12:23:02 +0200

    clusterctl/examples: write script for generating bootstrap manifests

--- clusterctl/examples/digitalocean/generate-yaml.sh
+++ clusterctl/examples/digitalocean/generate-yaml.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 set -e
 
-PROVIDERCOMPONENT_TEMPLATE_FILE=provider-components.yaml.template
-PROVIDERCOMPONENT_GENERATED_FILE=provider-components.yaml
-
 OVERWRITE=0
+OUTPUT_DIR=out
+
+PROVIDERCOMPONENT_TEMPLATE_FILE=provider-components.yaml.template
+PROVIDERCOMPONENT_GENERATED_FILE=${OUTPUT_DIR}/provider-components.yaml
+MACHINES_TEMPLATE_FILE=machines.yaml.template
+MACHINES_GENERATED_FILE=${OUTPUT_DIR}/machines.yaml
+CLUSTER_TEMPLATE_FILE=cluster.yaml.template
+CLUSTER_GENERATED_FILE=${OUTPUT_DIR}/cluster.yaml
+
+REGION=fra1
+CLUSTER_NAME=test-1
+MASTER_NAME=digitalocean-fra1-master-1
+NODE_NAME=digitalocean-fra1-node-1
 
 SCRIPT=$(basename $0)
 while test $# -gt 0; do
@@ -38,17 +48,21 @@ if [ $OVERWRITE -ne 1 ] && [ -f $PROVIDERCOMPONENT_GENERATED_FILE ]; then
   exit 1
 fi
 
-OS=$(uname)
-if [[ "$OS" =~ "Linux" ]]; then
-elif [[ "$OS" =~ "Darwin" ]]; then
-else
-  echo "Unrecognized OS : $OS"
-  exit 1
-fi
+mkdir -p ${OUTPUT_DIR}
 
 cat $PROVIDERCOMPONENT_TEMPLATE_FILE \
   > $PROVIDERCOMPONENT_GENERATED_FILE
-
 echo "Done generating $PROVIDERCOMPONENT_GENERATED_FILE"
-echo "You will still need to generate the cluster.yaml and machines.yaml"
+
+cat $MACHINES_TEMPLATE_FILE \
+    | sed -e "s/\$REGION/$REGION/" \
+    | sed -e "s/\$MASTER_NAME/$MASTER_NAME/" \
+    | sed -e "s/\$NODE_NAME/$NODE_NAME/" \
+  > $MACHINES_GENERATED_FILE
+echo "Done generating $MACHINES_GENERATED_FILE"
+
+cat $CLUSTER_TEMPLATE_FILE \
+    | sed -e "s/\$CLUSTER_NAME/$CLUSTER_NAME/" \
+  > $CLUSTER_GENERATED_FILE
+echo "Done generating $CLUSTER_GENERATED_FILE"
 
