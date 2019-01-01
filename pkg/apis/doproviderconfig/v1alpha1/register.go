147047c0b4e84de13eeaaa98a5756008a322b53a
Marko Mudrinić <mudrinic.mare@gmail.com>
2019-01-01 18:48:05 +0100

    Add new ProviderConfig types based on CRDs

---
+++ pkg/apis/doproviderconfig/v1alpha1/register.go
@@ -0,0 +1,120 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen=package,register
+// +k8s:conversion-gen=sigs.k8s.io/cluster-api-provider-digitalocean/pkg/apis/doproviderconfig
+// +k8s:openapi-gen=true
+// +k8s:defaulter-gen=TypeMeta
+package v1alpha1
+
+import (
+	"bytes"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	clusterv1 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/scheme"
+)
+
+// DigitalOceanProviderConfigCodec is codec for decoding and encoding DigitalOcean ProviderConfig.
+// +k8s:deepcopy-gen=false
+type DigitalOceanProviderConfigCodec struct {
+	encoder runtime.Encoder
+	decoder runtime.Decoder
+}
+
+var (
+	// SchemeGroupVersion represents the API group and version
+	SchemeGroupVersion = schema.GroupVersion{Group: "doproviderconfig.k8s.io", Version: "v1alpha1"}
+	// SchemeBuilder collects functions that add things to a scheme.
+	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+)
+
+func init() {
+	SchemeBuilder.Register(&DigitalOceanMachineProviderConfig{})
+}
+
+func NewScheme() (*runtime.Scheme, error) {
+	return SchemeBuilder.Build()
+}
+
+func NewCodec() (*DigitalOceanProviderConfigCodec, error) {
+	scheme, err := NewScheme()
+	if err != nil {
+		return nil, err
+	}
+	codecFactory := serializer.NewCodecFactory(scheme)
+	encoder, err := newEncoder(&codecFactory)
+	if err != nil {
+		return nil, err
+	}
+	codec := DigitalOceanProviderConfigCodec{
+		encoder: encoder,
+		decoder: codecFactory.UniversalDecoder(SchemeGroupVersion),
+	}
+	return &codec, nil
+}
+
+func (codec *DigitalOceanProviderConfigCodec) DecodeFromProviderConfig(providerConfig clusterv1.ProviderSpec, out runtime.Object) error {
+	if providerConfig.Value != nil {
+		_, _, err := codec.decoder.Decode(providerConfig.Value.Raw, nil, out)
+		if err != nil {
+			return fmt.Errorf("unable to decode from ProviderConfig: %v", err)
+		}
+	}
+	return nil
+}
+
+func (codec *DigitalOceanProviderConfigCodec) EncodeToProviderConfig(in runtime.Object) (*clusterv1.ProviderSpec, error) {
+	var buf bytes.Buffer
+	if err := codec.encoder.Encode(in, &buf); err != nil {
+		return nil, fmt.Errorf("unable to encode to ProviderConfig: %v", err)
+	}
+	return &clusterv1.ProviderSpec{
+		Value: &runtime.RawExtension{Raw: buf.Bytes()},
+	}, nil
+}
+
+func (codec *DigitalOceanProviderConfigCodec) EncodeProviderStatus(in runtime.Object) (*runtime.RawExtension, error) {
+	var buf bytes.Buffer
+	if err := codec.encoder.Encode(in, &buf); err != nil {
+		return nil, fmt.Errorf("unable to encode to ProviderConfig: %v", err)
+	}
+
+	return &runtime.RawExtension{Raw: buf.Bytes()}, nil
+}
+
+func (codec *DigitalOceanProviderConfigCodec) DecodeProviderStatus(providerStatus *runtime.RawExtension, out runtime.Object) error {
+	if providerStatus != nil {
+		_, _, err := codec.decoder.Decode(providerStatus.Raw, nil, out)
+		if err != nil {
+			return fmt.Errorf("unable to decode from ProviderConfig: %v", err)
+		}
+	}
+	return nil
+}
+
+func newEncoder(codecFactory *serializer.CodecFactory) (runtime.Encoder, error) {
+	serializerInfos := codecFactory.SupportedMediaTypes()
+	if len(serializerInfos) == 0 {
+		return nil, fmt.Errorf("unable to find any serlializers")
+	}
+	encoder := codecFactory.EncoderForVersion(serializerInfos[0].Serializer, SchemeGroupVersion)
+	return encoder, nil
+}
