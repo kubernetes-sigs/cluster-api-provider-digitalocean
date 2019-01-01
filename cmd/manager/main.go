37b0063ead863fd4c99433c39c5ac340eb36f220
Marko Mudrinić <mudrinic.mare@gmail.com>
2019-01-01 18:49:37 +0100

    Implement Manager

---
+++ cmd/manager/main.go
@@ -0,0 +1,87 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"flag"
+	"log"
+
+	"k8s.io/klog"
+	"sigs.k8s.io/cluster-api-provider-digitalocean/pkg/cloud/digitalocean/actuators/machine"
+	"sigs.k8s.io/cluster-api-provider-digitalocean/pkg/cloud/digitalocean/actuators/machine/machinesetup"
+	"sigs.k8s.io/cluster-api-provider-digitalocean/pkg/controller"
+	"sigs.k8s.io/cluster-api/pkg/apis/cluster/common"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
+
+	"sigs.k8s.io/cluster-api-provider-digitalocean/pkg/apis"
+	clusterapis "sigs.k8s.io/cluster-api/pkg/apis"
+)
+
+var (
+	machineSetupConfig = flag.String("config", "/etc/machineconfig/machines_config.yaml", "path to the machine setup config")
+)
+
+func main() {
+	flag.Parse()
+
+	// Get a config to talk to the apiserver
+	cfg, err := config.GetConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Setup a Manager
+	mgr, err := manager.New(cfg, manager.Options{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	configWatch, err := machinesetup.NewConfigWatch(*machineSetupConfig)
+	if err != nil {
+		klog.Fatalf("Could not create config watch: %v", err)
+	}
+
+	machine.MachineActuator, err = machine.NewMachineActuator(machine.ActuatorParams{
+		MachineSetupConfigGetter: configWatch,
+		EventRecorder:            mgr.GetRecorder("digitalocean-controller"),
+		Client:                   mgr.GetClient(),
+		Scheme:                   mgr.GetScheme(),
+	})
+	if err != nil {
+		log.Fatalf("Error creating cluster provisioner for google : %v", err)
+	}
+
+	common.RegisterClusterProvisioner("digitalocean", machine.MachineActuator)
+
+	// Setup Scheme for all resources
+	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := clusterapis.AddToScheme(mgr.GetScheme()); err != nil {
+		log.Fatal(err)
+	}
+	// Setup all Controllers
+	if err := controller.AddToManager(mgr); err != nil {
+		log.Fatal(err)
+	}
+
+	// Start the Manager
+	log.Fatal(mgr.Start(signals.SetupSignalHandler()))
+}
