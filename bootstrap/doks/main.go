13a8e78598e6c89b269205cfa3a95479b6ccc9fc
fst <fabian.stegemann@gmx.net>
2021-06-24 17:21:17 +0200

    generate DOKSBootstrapConfig API

--- bootstrap/doks/main.go
+++ bootstrap/doks/main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	bootstrapv1alpha4 "sigs.k8s.io/cluster-api-provider-digitalocean/api/v1alpha4"
+	"sigs.k8s.io/cluster-api-provider-digitalocean/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(bootstrapv1alpha4.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +78,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.DOKSBootstrapConfigReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DOKSBootstrapConfig")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
