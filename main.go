a2ec91147ab63dba49195500e1c0804a7c1edea3
fst <fabian.stegemann@gmx.net>
2021-06-24 08:49:18 +0200

    generate DOKSNodePool

--- main.go
+++ main.go
@@ -164,6 +164,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DOKSCluster")
 		os.Exit(1)
 	}
+	if err = (&controllers.DOKSNodePoolReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DOKSNodePool")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddReadyzCheck("ping", healthz.Ping); err != nil {
